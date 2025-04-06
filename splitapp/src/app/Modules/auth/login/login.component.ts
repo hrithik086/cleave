f3cb6beb5d304186f8ee6065d2046052abd8056b
hrithik <hrithik086@gmail.com>
2025-04-06 16:13:30 +0530

    Google SSO integrated in the frontend

--- splitapp/src/app/Modules/auth/login/login.component.ts
+++ splitapp/src/app/Modules/auth/login/login.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+// This declaraion is part of the Angular project.
+declare var google: any;
+
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/Services/Auth/auth.service';
 import { LoginDto } from 'src/app/Services/Auth/Models/LoginDto';
+import { UserDto } from 'src/app/Services/Auth/Models/UserDto';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  ngOnInit(): void {
+    google.accounts.id.initialize({
+      client_id: "1021269737526-ehrr84a5ffl308kudld700nn6vbrpc8b.apps.googleusercontent.com",
+      callback : (response: any) => {
+        this.handleGoogleSsoLogin(response);
+      }
+    })
+
+    google.accounts.id.renderButton(document.getElementById("googleSignIn"), {
+      theme: "filled_blue",
+      size: "large",
+      shape: "rectangular",
+      text: "signin_with",
+      logo_alignment: "left"
+    })
+  }
 
   email: string = '';
   password: string = '';
@@ -23,6 +43,15 @@ export class LoginComponent {
     this.authService.loginUser(loginDetails).subscribe(data => {
       console.log(data);
     })
+  }
+
+  private decodeJwtToken(token: string) {
+    return JSON.parse(atob(token.split('.')[1]));
+  }
+
+  handleGoogleSsoLogin(response:any){
+    const payload = this.decodeJwtToken(response.credential);
+    sessionStorage.setItem('user', JSON.stringify(payload));
   }
 
 }
